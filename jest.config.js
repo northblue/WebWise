1faef3b9374eae262619d0b12f16f3d063fb811b
Bin Li <northblue@gmail.com>
2024-09-23 21:37:18 +1000

    Added sample test case

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1', // 添加路径别名
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['<rootDir>/test/**/*.test.(js|jsx|ts|tsx)'],
+};
+
+module.exports = createJestConfig(customJestConfig);
